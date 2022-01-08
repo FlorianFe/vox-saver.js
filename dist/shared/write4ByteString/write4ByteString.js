064b9cdc8eb73f33d5d1195020ce90a85dfea051
FlorianFe <webtutorialdesign@gmail.com>
2022-01-08 19:36:11 +0100

    cleanup

--- dist/shared/write4ByteString/write4ByteString.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var write4ByteString = function (text) {
-    if (text.length !== 4)
-        throw Error("4 Byte String should have 4 characters");
-    return text.split("").map(function (char) { return char.charCodeAt(0); });
-};
-module.exports = write4ByteString;
